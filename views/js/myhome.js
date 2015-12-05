799a3690e4ea8d5c440613d2318a33d01fe53205
chaozc <zichen.chao@gmail.com>
2015-12-05 17:17:57 -0500

    test

--- views/js/myhome.js
+++ views/js/myhome.js
@@ -59,8 +59,8 @@ function feedDataModal(){
 
 function editProfile(){
 	document.getElementById("name").innerHTML = document.getElementById("set-name").value+'<img class="img-hover pull-right" src="img/gear39.png" data-toggle="modal" data-target="#myModal" onclick="feedDataModal()">';
-	document.getElementById("loc").innerHTML = '<img src="img/pin56.png"> '+document.getElementById("set-loc").value;
-	document.getElementById("edu").innerHTML = '<img src="img/graduation-cap2.png"> '+document.getElementById("set-edu").value;
+	document.getElementById("loc").innerHTML = document.getElementById("set-loc").value;
+	document.getElementById("edu").innerHTML = document.getElementById("set-edu").value;
 
 	//document.getElementById("show").innerHTML = document.getElementById("upload").value;
 /*
